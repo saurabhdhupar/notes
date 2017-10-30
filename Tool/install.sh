d5aed929c03b8897cdcb0ace93b2039f0c570984
Chyroc <chen_yunpeng@foxmail.com>
2017-10-30 10:57:14 +0800

    add brew quick look install

--- Tool/install.sh
+++ Tool/install.sh
@@ -3,3 +3,20 @@
 brew update
 
 brew install doitlive
+
+# quick look
+brew cask install qlmarkdown
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install qlimagesize
+brew cask install webpquicklook
+brew cask install suspicious-package
+# brew cask install quicklookase
+# brew cask install qlvideo
+# brew cask install provisionql
+# brew cask install quicklookapk
+# brew cask install quicklook-pat
